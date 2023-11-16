8a8533aaceaaf01fe8191ca3c1f255d9f3914392
Mian Nouman Ahmed <numan1836@gmail.com>
2023-11-16 19:16:01 +0500

    Error Handling

--- BACKEND/routes/places-routes.js
+++ BACKEND/routes/places-routes.js
@@ -18,9 +18,17 @@ const DUMMY_PLACES = [
 
 router.get("/:pid", (req, res, next) => {
   const placeId = req.params.pid;
+
   const place = DUMMY_PLACES.find((p) => {
     return p.id == placeId;
   });
+
+  if (!place) {
+    const error = new Error("Could not find a Place for Provided Id.");
+    error.code = 404;
+    throw error;
+  }
+
   res.json({
     place,
   });
@@ -32,6 +40,12 @@ router.get("/user/:uid", (req, res, next) => {
   const place = DUMMY_PLACES.find((p) => {
     return p.creator == userId;
   });
+
+  if (!place) {
+    const error = new Error("Could not find a Place for Provided User Id.");
+    error.code = 404;
+    return next(error);
+  }
 
   res.json({
     place,
