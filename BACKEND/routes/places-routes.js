ce84c893192e8006b3c703345242f99ec0160b01
Mian Nouman Ahmed <numan1836@gmail.com>
2023-11-16 18:27:50 +0500

    find places by user id

--- BACKEND/routes/places-routes.js
+++ BACKEND/routes/places-routes.js
@@ -21,7 +21,18 @@ router.get("/:pid", (req, res, next) => {
   const place = DUMMY_PLACES.find((p) => {
     return p.id == placeId;
   });
-  console.log("GET Request in Places");
+  res.json({
+    place,
+  });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator == userId;
+  });
+
   res.json({
     place,
   });
