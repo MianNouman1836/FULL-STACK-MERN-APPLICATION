839b79feb3e4c54e0230581e5a03c156436e36a0
Mian Nouman Ahmed <numan1836@gmail.com>
2023-11-17 16:11:07 +0500

    Adding PATCH Routes

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+<<<<<<< HEAD
 const placesRoutes = require('./routes/places-routes');
 const HttpError = require('./models/http-error');
+=======
+const placesRoutes = require("./routes/places-routes");
+>>>>>>> parent of d9f84c4 (Handling Errors for UnSupported Routes)
 
 const app = express();
 
@@ -10,11 +14,14 @@ app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes); // => /api/places...
 
+<<<<<<< HEAD
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
 
+=======
+>>>>>>> parent of d9f84c4 (Handling Errors for UnSupported Routes)
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
