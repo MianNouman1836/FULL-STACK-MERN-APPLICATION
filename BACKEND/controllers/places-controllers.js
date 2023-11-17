c61eb6ac35df483d174587f7d53e73462aa26e2e
Mian Nouman Ahmed <numan1836@gmail.com>
2023-11-17 18:54:32 +0500

    Validating Patch Requests & User Routes

--- BACKEND/controllers/places-controllers.js
+++ BACKEND/controllers/places-controllers.js
@@ -53,7 +53,6 @@ const getPlacesByUserId = (req, res, next) => {
 const createPlace = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors);
     throw new HttpError("Invalid Inputs Passed, Please Check your Data!", 422);
   }
 
@@ -75,6 +74,11 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError("Invalid Inputs Passed, Please Check your Data!", 422);
+  }
+
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
@@ -90,6 +94,11 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+
+  if (!DUMMY_PLACES.find((p) => p.id == placeId)) {
+    throw new HttpError("Could not Find a Place for that ID!", 404);
+  }
+
   DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
   res.status(200).json({ message: "Deleted place." });
 };
