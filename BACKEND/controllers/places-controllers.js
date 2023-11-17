2731715d5b31aa0444d5ce7dd5f38b5fdeab4222
Mian Nouman Ahmed <numan1836@gmail.com>
2023-11-17 18:34:22 +0500

    Validating API Input (Request Bodies)

--- BACKEND/controllers/places-controllers.js
+++ BACKEND/controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const uuid = require("uuid/v4");
+const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 
@@ -50,7 +51,14 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid Inputs Passed, Please Check your Data!", 422);
+  }
+
   const { title, description, coordinates, address, creator } = req.body;
+
   // const title = req.body.title;
   const createdPlace = {
     id: uuid(),
