8ee33bb132866567ff96a83ae390624f3eb7aa33
Mian Nouman Ahmed <numan1836@gmail.com>
2023-11-17 17:57:48 +0500

    Setting Up the User Routes (Signup, Login, Get Users)

---
+++ BACKEND/controllers/users-controllers.js
@@ -0,0 +1,54 @@
+const uuid = require("uuid/v4");
+
+const HttpError = require("../models/http-error");
+
+const DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "Mian Nouman Ahmed",
+    email: "numan1836@gmail.com",
+    password: "test123",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find((u) => u.email == email);
+  if (hasUser) {
+    throw new HttpError("Could not Create User, email Already Exists!", 422);
+  }
+
+  const createdUser = {
+    id: uuid(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_USERS.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find((u) => u.email == email);
+  if (!identifiedUser || identifiedUser.password != password) {
+    throw new HttpError(
+      "Could not Find Identified User, Credentials Seem to be Wrong!",
+      401
+    );
+  }
+
+  res.json({ message: "Logged In!" });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
