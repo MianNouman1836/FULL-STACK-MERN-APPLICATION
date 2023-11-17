2ee455c81b112f0be78c06eddb49c75801002430
Mian Nouman Ahmed <numan1836@gmail.com>
2023-11-17 16:39:51 +0500

    Deleting Place

--- BACKEND/controllers/places-controller.js
+++ BACKEND/controllers/places-controller.js
@@ -1,6 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
 
 const HttpError = require("../models/http-error");
+const { json } = require('body-parser');
 
 const DUMMY_PLACES = [
   {
@@ -86,7 +87,11 @@ const updatePlace = (req, res, next) => {
 };
 
 const deletePlace = (req, res, next) => {
-  
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+  res.status(200),json({
+    message: "Place Deleted.",
+  })
 };
 
 exports.getPlaceById = getPlaceById;
