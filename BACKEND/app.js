956778063a6adc2fd43466b316a027bc649acca0
Mian Nouman Ahmed <numan1836@gmail.com>
2023-11-20 15:24:19 +0500

    installing mongoose

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
